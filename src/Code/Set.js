abd83f05ef1d57cb43862f54a9feefeb9ca2988e
iamdarshangowda <iamdarshangowda@gmail.com>
2022-07-13 21:18:12 +0530

    deleted code folder

--- src/Code/Set.js
+++
@@ -1,7 +0,0 @@
-function char(str1, str2) {
-  str1 = str1.toLowerCase();
-  str2 = str2.toLowerCase();
-  let op1 = [...str1].filter((item) => !str2.includes(item)).join("");
-  let op2 = [...str2].filter((item) => !str1.includes(item)).join("");
-  return `Output1 is ${op1 || null}, Output2 is ${op2 || null}`;
-}
